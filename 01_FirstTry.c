39f47d8e07c5f668ca520a74b1dcb4089650fa00
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-01-17 16:28:40 +0530

    Initial commit

---
+++ 01_FirstTry.c
@@ -0,0 +1,52 @@
+#define MAX 3
+
+int main()
+{
+    // int roll1, roll2, roll3;
+    // int marks1, marks2, marks3;
+    // int std1, std2, std3;
+
+    int roll[MAX], marks[MAX], std[MAX]
+    printf("Enter student 1 Name: ")
+    scanf(...)
+
+    if ( st1.getRoll() = 10)
+    {
+        //Operations
+    }
+
+    st1.age
+    st1.getAge()
+
+}
+
+class student
+{
+    // int age;
+    DOB dob;
+
+    int getAge()
+    {
+        int age = sysTime -  dob;
+        return age;
+    }
+}
+
+POS - ARM - Limited computing resources X
+            Limited Memory
+
+class Math
+{
+    int Add(int, int) -->  Add_int_int
+
+    float Add(float, float)  --> Add_Float_Float
+
+    string Add(string, string)
+};
+
+// Polymorphism - Same invocation leads to different behaviour 
+//                    depending on the type of object that received the signal
+
+
+template<T>
+T Add (T, T)
---
+++ 02_FirstPython.py
@@ -0,0 +1,9 @@
+print("Hello")
+
+def Test1(name):
+    print("Hello", name)
+
+def main():
+    Test1("Manish")
+
+main()
